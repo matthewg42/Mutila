e045f8a297a7ed56a6ce0b8c8816d7954468b270
Matthew Gates <matthewg42@gmail.com>
2016-12-19 16:54:24 +0000

    compare VDiv Samplers

---
+++ examples/VDivSamplerExample/VDivSamplerExample.ino
@@ -0,0 +1,32 @@
+#include <BufferedVDivSampler.h>
+#include <EMAVDivSampler.h>
+
+// Compare two methods for smoothing VDIV readings.
+
+BufferedVDivSampler bufSampler(A0, 20, 200, 5.0, 0, 10);
+EMAVDivSampler emaSampler(A0, 20, 200, 5.0, 0, 0.4);
+
+void setup()
+{
+    Serial.begin(115200);
+    bufSampler.begin();
+    emaSampler.begin();
+    // Settle down
+    delay(300);
+    Serial.println("millis,raw,buffer,ema");
+}
+
+void loop()
+{
+    bufSampler.update();
+    emaSampler.update();
+    Serial.print(millis());
+    Serial.print(',');
+    Serial.print(emaSampler.lastVolts());
+    Serial.print(',');
+    Serial.print(bufSampler.averageVolts());
+    Serial.print(',');
+    Serial.println(emaSampler.averageVolts());
+    delay(20);
+}
+
