6fdc19467f94cbb516a84c4b36820c9f57f6f3b9
Matthew Gates <matthewg42@gmail.com>
2016-12-21 01:09:08 +0000

    docs

--- DFPlayerMini.h
+++ DFPlayerMini.h
@@ -27,28 +27,29 @@ struct DFPResponse {
  */
 class DFPlayerMini {
 public:
-    enum Cmd {
-        Undefined       = 0x00,
-        Next            = 0x01,
-        Prev            = 0x02,
-        SetVolume       = 0x06,
-        SetEqalizer     = 0x07,
-        SetDevice       = 0x09,
-        Sleep           = 0x0A,
-        Reset           = 0x0C,
-        PlayN           = 0x0D,
-        Pause           = 0x0E,
-        PlayNtfMp3      = 0x12,
-        Stop            = 0x16,
-        SingleLoop      = 0x19,
-        GetStatus       = 0x42,
-        GetVolume       = 0x43,
-        GetUSum         = 0x47,
-        GetTfSum        = 0x48,
-        GetFlashSum     = 0x49,
-        GetUCurrent     = 0x4B,
-        GetTfCurrent    = 0x4C,
-        GetFlashCurrent = 0x4D
+    enum Cmd {                  
+        Undefined       = 0x00, 
+        Next            = 0x01, //!< Play next track
+        Prev            = 0x02, //!< Play previous track
+        PlayPhysical    = 0x03, //!< Play track arg
+        SetVolume       = 0x06, //!< Set volume to arg (0-30)
+        SetEqalizer     = 0x07, //!< Set EQ, arg 0-5 (normal=0, pop, rock, jazz, classical, bass) 
+        SetDevice       = 0x09, //!< Set device, arg 1-5 (U, SD, AUX, SLEEP, FLASH)
+        Sleep           = 0x0A, //!< Enter low power sleep mode
+        Reset           = 0x0C, //!< Exit sleep mode
+        Play            = 0x0D, //!< Play track arg
+        Pause           = 0x0E, //!< Pause playback
+        PlayTfMp3       = 0x12, //!< Play track arg from Tf
+        Stop            = 0x16, //!< Stop playback
+        SingleLoop      = 0x19, //!< Loop track arg = boolean !state
+        GetStatus       = 0x42, //!< Get some meaningless status code
+        GetVolume       = 0x43, //!< Get volume setting
+        GetUSum         = 0x47, //!< Get file count U
+        GetTfSum        = 0x48, //!< Get file count Tf
+        GetFlashSum     = 0x49, //!< Get file count Flash
+        GetUCurrent     = 0x4B, //!< Get file current U
+        GetTfCurrent    = 0x4C, //!< Get file current Tf
+        GetFlashCurrent = 0x4D  //!< Get file current Flash
     };
 
     //! Constructor
