1cf1114ffc1fc097ee17f089dc115eeb9b9f27ce
Matthew Gates <matthewg42@gmail.com>
2017-05-31 11:02:49 +0100

    Add DBF (printf style)
    This isn't availabale in the stock arduino Serial object, but
    is available for some other boards, like the ESP8266, so having
    a macro is useful...

--- MutilaDebug.h
+++ MutilaDebug.h
@@ -18,12 +18,18 @@
 #define _DB(...) \
             do { if (MUTILADEBUG) Serial.print(__VA_ARGS__); } while (0)
 
+#define _DBF(...) \
+            do { if (MUTILADEBUG) Serial.printf(__VA_ARGS__); } while (0)
+
 #define _DBLN(...) \
             do { if (MUTILADEBUG) Serial.println(__VA_ARGS__); } while (0)
 
 // For use in user apps...
 #define DB(...) \
             do { if (DEBUG) Serial.print(__VA_ARGS__); } while (0)
+
+#define DBF(...) \
+            do { if (DEBUG) Serial.printf(__VA_ARGS__); } while (0)
 
 #define DBLN(...) \
             do { if (DEBUG) Serial.println(__VA_ARGS__); } while (0)
