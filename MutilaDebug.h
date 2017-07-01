10e22239395cd56296cb4d64c33f629c70da6bf4
Matthew Gates <matthewg42@gmail.com>
2017-07-01 14:30:26 +0100

    make debug stream configurable

--- MutilaDebug.h
+++ MutilaDebug.h
@@ -2,6 +2,14 @@
 
 #include <Arduino.h>
 
+#ifndef DEBUGSTREAM
+#define DEBUGSTREAM Serial
+#endif
+
+#ifndef DEBUGBAUD
+#define DEBUGBAUD 115200
+#endif
+
 #ifdef DEBUG
 #define DEBUG 1
 #else
@@ -16,21 +24,23 @@
 
 // This for internal Mutila use...
 #define _DB(...) \
-            do { if (MUTILADEBUG) Serial.print(__VA_ARGS__); } while (0)
+            do { if (MUTILADEBUG) DEBUGSTREAM.print(__VA_ARGS__); } while (0)
 
 #define _DBF(...) \
-            do { if (MUTILADEBUG) Serial.printf(__VA_ARGS__); } while (0)
+            do { if (MUTILADEBUG) DEBUGSTREAM.printf(__VA_ARGS__); } while (0)
 
 #define _DBLN(...) \
-            do { if (MUTILADEBUG) Serial.println(__VA_ARGS__); } while (0)
+            do { if (MUTILADEBUG) DEBUGSTREAM.println(__VA_ARGS__); } while (0)
 
 // For use in user apps...
+#define DBBEGIN do { DEBUGSTREAM.begin(DEBUGBAUD); } while (0)
+
 #define DB(...) \
-            do { if (DEBUG) Serial.print(__VA_ARGS__); } while (0)
+            do { if (DEBUG) DEBUGSTREAM.print(__VA_ARGS__); } while (0)
 
 #define DBF(...) \
-            do { if (DEBUG) Serial.printf(__VA_ARGS__); } while (0)
+            do { if (DEBUG) DEBUGSTREAM.printf(__VA_ARGS__); } while (0)
 
 #define DBLN(...) \
-            do { if (DEBUG) Serial.println(__VA_ARGS__); } while (0)
+            do { if (DEBUG) DEBUGSTREAM.println(__VA_ARGS__); } while (0)
 
