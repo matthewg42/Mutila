3fb38dd82e3329506b6ad9a195c6e73d8da9314b
Matthew Gates <matthewg42@gmail.com>
2017-07-01 15:50:28 +0100

    fix rounding problem

--- VIPCalculation.cpp
+++ VIPCalculation.cpp
@@ -2,7 +2,7 @@
 
 float vDivVolts(const int value, const uint16_t r1KOhm, const uint16_t r2KOhm, const float vRef)
 {
-    return (value*vRef*(r1KOhm+r2KOhm)) / (1024*r1KOhm);
+    return (value*vRef*(r1KOhm+r2KOhm)) / (1024.0*r1KOhm);
 }
 
 float currentConversion(const int value, const float vSupply, const float vRef, const float iOffset)
