fed54d88bff90f57929882f8f65d04cfae27d9a9
Matthew Gates <matthewg42@gmail.com>
2017-12-05 21:28:36 +0000

    doc tweaks

--- src/DebouncedButton.h
+++ src/DebouncedButton.h
@@ -4,13 +4,13 @@
 #include "Mutila.h"
 #include "RawButton.h"
 
-/*! \brief Timeslice-based button with debouncing
+/*! Timeslice-based button with debouncing
  *
- * DebouncedButton reduces / eliminates bouncing (multiple press
- * events registing close together with only one physical press).
+ *  DebouncedButton reduces / eliminates bouncing (multiple press
+ *  events registing close together with only one physical press).
  *
- * The update() method must be called frequently (usually from 
- * loop()) to work properly.
+ *  The update() method must be called frequently (usually from 
+ *  loop()) to work properly.
  */
 class DebouncedButton : public RawButton {
 public:
@@ -21,77 +21,74 @@ public:
     static const uint16_t DefaultButtonRepeatMs = 350;
 
 public:
-    /*! Constructor
+    /*! Constructor.
      *
-     * \param pin the pin used for this button
-     * \param pullup if true pin==LOW means on, else pin==HIGH is on
+     *  \param pin the pin used for this button.
+     *  \param pullup if true pin==LOW means on, else pin==HIGH is on.
      */
     DebouncedButton(uint8_t pin, bool pullup=true);
 
-    /*! Initialization
+    /*! Initialization.
      *
-     * \param threshold how many tests of the pin must match for a state change
-     * \param delay number of ms between tests of pin state
+     *  \param threshold how many tests of the pin must match for a state change.
+     *  \param delay number of ms between tests of pin state.
      *
-     * Should be called from setup(), or at any rate, before
-     * other members are called. This will set the pinMode. The
-     * minimum time it takes for button presses / released to register
-     * is threshold * delay.
+     *  Should be called from setup(), or at any rate, before
+     *  other members are called. This will set the pinMode. The
+     *  minimum time it takes for button presses / released to register
+     *  is threshold * delay.
      */
     void begin(uint8_t threshold=DefaultThreshold, uint8_t delay=DefaultButtonDelay);
 
-    /*! Allocate Timeslice
+    /*! Allocate timeslice.
      *
-     * This method must be called frequently - usually from loop()
+     *  This method must be called frequently - usually from loop().
      */
     void update();
 
-    /*! Test instantaneous input state (with debouncing)
+    /*! Test instantaneous input state (with debouncing).
      *
-     * \return true if the button is pushed, else false.
-     *
+     *  \return true if the button is pushed, else false.
      */
     bool on();
 
-    /*! Test if the button has been pushed since the last time it was off
+    /*! Test if the button has been pushed since the last time it was off.
      *
-     * The idea is that you can call pushed() lots of times in a tight loop
-     * and only get one true return pre press of the button regardless of
-     * how many times you test.
+     *  The idea is that you can call pushed() lots of times in a tight loop
+     *  and only get one true return pre press of the button regardless of
+     *  how many times you test.
      */
     bool pushed();
 
     /*! Test if the button has been pushed and released.
      *
-     * After returning a non-zero value (i.e. a tap was registered), the
-     * state will be reset. Only the last tap duration will be returned.
+     *  After returning a non-zero value (i.e. a tap was registered), the
+     *  state will be reset. Only the last tap duration will be returned.
      *
-     * \return length of last tap in ms if the button has been tapped, else 0.
-     *
+     *  \return length of last tap in ms if the button has been tapped, else 0.
      */
     uint32_t tapped();
 
-    /*! Test if held on for extended period
+    /*! Test if held on for extended period.
      *
-     * \param ms time in ms the button has to have been on for to be considered held
+     *  \param ms time in ms the button has to have been on for to be considered held
      *
-     * \return true if the button is pushed and has been for longer 
-     *         than specified time.
-     *
+     *  \return true if the button is pushed and has been for longer 
+     *          than specified time.
      */
     bool held(uint16_t ms=DefaultHeldMs);
 
     /*! Get periodic press results when button is held.
      *
-     * \param initialMs time between first press and first repeat in ms
-     * \param repeatMs time between subsequent releats
-     * \return true when pushed every so often...
+     *  \param initialMs time between first press and first repeat in ms.
+     *  \param repeatMs time between subsequent releats.
+     *  \return true when pushed every so often...
      */
     bool repeat(uint16_t initialMs=DefaultButtonRepeatInitialMs, uint16_t repeatMs=DefaultButtonRepeatMs);
 
     /*! Explicitly set the state of the button.
      *
-     * \param newState the new state to have
+     *  \param newState the new state to have.
      */
     void setState(bool newState);
 
