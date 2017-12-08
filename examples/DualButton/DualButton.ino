28031f40c7a6c147b549938ebd89fc621f5fd9c5
Matthew Gates <matthewg42@gmail.com>
2017-12-08 13:30:55 +0000

    esp compat

--- examples/DualButton/DualButton.ino
+++ examples/DualButton/DualButton.ino
@@ -3,9 +3,15 @@
 #include <Millis.h>
 #include <MutilaDebug.h>
 
+#if defined(ARDUINO_ESP8266_NODEMCU) 
+const uint8_t Button1Pin = D1;
+const uint8_t Button2Pin = D2;
+#else
 const uint8_t Button1Pin = 6;
 const uint8_t Button2Pin = 5;
-const uint8_t OutputMs = 50;
+#endif
+
+const uint8_t OutputMs = 100;
 uint32_t LastDb = 0;
 
 // One button with LOW == pushed
@@ -17,15 +23,15 @@ DebouncedButton Button2(Button2Pin);
 // If either is pushed, the DualButton will register a press
 DualButton MyButton(&Button1, &Button2);
 
-
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("\n\nsetup() start");
     MyButton.begin();
     // Show we can handle Millis wrap
     addMillisOffset(0xFFFFF000);
     delay(300);
-    Serial.println("setup() complete");
+    Serial.println("setup() end");
 }
 
 void loop()
