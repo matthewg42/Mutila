5429929762b6f74aec3124becffc4039cb1e20e7
Matthew Gates <matthewg42@gmail.com>
2017-12-08 15:09:53 +0000

    esp compat

--- examples/SonicRanger/SonicRanger.ino
+++ examples/SonicRanger/SonicRanger.ino
@@ -3,26 +3,37 @@
 #include <SonicRanger.h>
 #include <Millis.h>
 
+#if defined(ARDUINO_ESP8266_NODEMCU) 
+const uint8_t TriggerPin = D3;
+const uint8_t EchoPin = D0;
+#else
 const uint8_t TriggerPin = 3;
 const uint8_t EchoPin = 4;
+#endif
 
 SonicRanger Ranger(TriggerPin, EchoPin);
 
 void setup()
 {
     Serial.begin(115200);
-    Ranger.begin();
+    Serial.println("setup() start");
+
     // Show we can handle Millis wrap
     addMillisOffset(0xFFFFF000);
-    delay(300);
-    Serial.println("setup() complete");
+
+    // Initialize our ranger
+    Ranger.begin();
+
+    Serial.println("setup() end");
 }
 
 void loop()
 {
-    unsigned long before = Millis();
+    // Get the range, and record time before and after to 
+    // work out how long the getRange() call takes
+    uint32_t before = Millis();
     uint16_t cm = Ranger.getRange();
-    unsigned long after = Millis();
+    uint32_t after = Millis();
 
     Serial.print("Millis=0x");
     Serial.print(Millis(), HEX);
