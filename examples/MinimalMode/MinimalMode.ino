23f5dd00e84c9a1587680b31e20ed5ca2cd220e9
Matthew Gates <matthewg42@gmail.com>
2017-12-08 14:56:52 +0000

    esp compat

--- examples/MinimalMode/MinimalMode.ino
+++ examples/MinimalMode/MinimalMode.ino
@@ -8,15 +8,21 @@ ModeOne mode;
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("setup() end");
+
     // Show we can handle Millis wrap
-    //addMillisOffset(0xFFFFF000);
+    addMillisOffset(0xFFFFF000);
+
+    // Initialize and start our mode
     mode.begin();
     mode.start();
-    Serial.println("setup() complete");
+
+    Serial.println("setup() end");
 }
 
 void loop()
 {
+    // Give our mode a time slice
     mode.update();
 }
 
