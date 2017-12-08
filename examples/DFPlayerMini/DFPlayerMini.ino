04c6624fd30a423f0592454e2b9f6f139c1266cb
Matthew Gates <matthewg42@gmail.com>
2017-12-08 16:09:22 +0000

    fix ESP DFPlayerMini example, add example audio

--- examples/DFPlayerMini/DFPlayerMini.ino
+++ examples/DFPlayerMini/DFPlayerMini.ino
@@ -3,19 +3,92 @@
 #include <Millis.h>
 #include <MutilaDebug.h>
 
+#if defined(ARDUINO_ESP8266_NODEMCU) 
+const uint8_t TxPin =       D6;
+const uint8_t RxPin =       D7;
+const uint8_t BusyPin =     D5;
+#else
 const uint8_t TxPin =       8;
 const uint8_t RxPin =       9;
 const uint8_t BusyPin =     10;
-const uint16_t BetweenMs =  200;
-const uint16_t Volume =     10;
+#endif
 
+// How long to wait between playing samples
+const uint16_t BetweenMs =  1500;
 
+// Volume from 1-30 (30 is loudest)
+const uint16_t Volume =     15;
+
+// This is based on the three example files from the audio directory
+const int16_t TrackCount = 3;
+
+// How often to print general status information
+const uint16_t OutputPeriodMs = 1000;
+
+// Global objects and variables
+uint32_t LastOutputMs = 0;
 SoftwareSerial SerialMP3(TxPin, RxPin);
 DFPlayerMini mp3(SerialMP3, BusyPin);
 uint32_t LastStop = 0;
 bool Playing = false;
-int16_t Count = 0;
 int16_t Track = 0;
+
+// Function prototypes so we can put functions in any order and
+// still build them with the Makefile.
+void setup();
+void loop();
+void query();
+void playNext();
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("\n\nsetup() start");
+
+    // Show that we can handle Millis overflow
+    SerialMP3.begin(9600);
+
+    // Initialize the mp3 player object
+    mp3.begin();
+
+    // Let IO pins settle
+    delay(200);  
+
+    // Set the volume
+    mp3.sendCmd(DFPlayerMini::SetVolume, Volume);
+
+    // Talk to the DFPlayer Mini device.  
+    // Also sets the number of tracks.
+    query();  
+
+    Serial.println("setup() end");
+}
+
+void loop() {
+    bool busy = mp3.busy();
+    uint32_t now = Millis();
+
+    if (DoEvery(OutputPeriodMs, LastOutputMs)) {
+        Serial.print("Millis=0x");
+        Serial.print(Millis(), HEX);
+        Serial.print(" busy pin=");
+        Serial.println(busy);
+    }
+
+    if (!busy) {
+        if (Playing) {
+            Serial.println("State change to: stopped");
+            LastStop = now;
+            Playing = false;
+        } 
+
+        if (MillisSince(LastStop, now) > BetweenMs) {
+            playNext();
+        }
+    } 
+    // This is to reduce the jitter we have if we don't use the BUSY pin
+    delay(100);
+}
 
 void query() {
     DFPResponse r;
@@ -36,7 +109,7 @@ void query() {
 
     r = mp3.query(DFPlayerMini::GetTfSum);
     Serial.print(" GetTfSum=");
-    if (r.status == DFPResponse::Ok) { Serial.print(r.arg); Count = r.arg; }
+    if (r.status == DFPResponse::Ok) { Serial.print(r.arg); }
     else { Serial.print("ERR"); }
 
     r = mp3.query(DFPlayerMini::GetUCurrent);
@@ -48,57 +121,17 @@ void query() {
     if (r.status == DFPResponse::Ok) { Serial.print(r.arg);  }
     else { Serial.print("ERR"); }
 
-    Serial.println(' ');
-
-}
-
-void setup()
-{
-    Serial.begin(115200);
-    Serial.println(F("\n\nS:setup"));
-    SerialMP3.begin(9600);
-    mp3.begin();
-
-    // Let IO pins settle
-    delay(200);  
-
-    // Don't shout
-    mp3.sendCmd(DFPlayerMini::SetVolume, Volume);
-
-    // Talk to the DFPlayer Mini device.  
-    // Also sets the number of tracks.
-    query();  
-    Serial.println(F("E:setup"));
+    Serial.println();
 }
 
 void playNext()
 {
-    Serial.print("Playing track ");
+    Serial.print("State change to: playing track ");
     Serial.println(Track);
     Playing = true;
     mp3.sendCmd(DFPlayerMini::PlayTf, Track++);
-    if (Track > Count) {
+    if (Track > TrackCount) {
         Track = 0;
     }
-}
-
-void loop() {
-    uint32_t now = Millis();
-    bool busy = mp3.busy();
-    Serial.print("busy: ");
-    Serial.println(busy);
-    if (!busy) {
-        if (Playing) {
-            Serial.println("stopped");
-            LastStop = now;
-            Playing = false;
-        } 
-
-        if (MillisSince(LastStop, now) > BetweenMs) {
-            playNext();
-        }
-    } 
-    // This is to reduce the jitter we have if we don't use the BUSY pin
-    delay(100);
 }
 
